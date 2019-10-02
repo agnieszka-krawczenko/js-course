7ec1b3e00b178c67dbc90aeb08533b73e6a285de
agnieszka-krawczenko <krawczenkoagnieszka@gmail.com>
2019-10-02 17:03:00 +0200

    Used the break and continue to get out of the loop in test.js

--- test.js
+++ test.js
@@ -18,6 +18,7 @@ else {
 }
 
 
+
 var myAge = 89
 
 if (myAge > 30){
@@ -30,12 +31,15 @@ if (myAge > 30){
     document.write("  You are not over 10!");  
 }
 
+
+
 var age = 35
 if (age >= 18 && age <=30){
     document.write("   You can come!");
 } else {
     document.write("   You aint coming!");
 }
+
 
 
 var age = 5;
@@ -45,7 +49,8 @@ while (age < 10) {
     age++;
 }
 document.write("You are now over 10");
-*/
+
+
 
 var links = document.getElementsByTagName("a");
 for (i = 1; i <= links.length ; i++) {
@@ -54,3 +59,15 @@ for (i = 1; i <= links.length ; i++) {
 
 }
 document.write("all links now looped");
+
+*/
+
+for ( i = 0; i < 10; i++ ){
+    if (i===5 || i===3){
+        continue;
+    }
+    console.log(i);
+    if(i === 7) {
+        break;
+    }
+} console.log("I have broken out of the loop");
