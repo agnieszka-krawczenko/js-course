a1fdcb676c38d90c47f10916b508cac1492c6ac4
agnieszka-krawczenko <krawczenkoagnieszka@gmail.com>
2019-10-02 22:59:45 +0200

    Created new.js to use some function in JS

---
+++ new.js
@@ -0,0 +1,10 @@
+function getAverage (a,b){
+
+var average = (a + b) / 2;
+console.log(average);
+return average;
+
+}
+
+var myResult = getAverage(7,12);
+console.log("the average is " + myResult);
