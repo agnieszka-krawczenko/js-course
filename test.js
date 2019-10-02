0e85f130d98f55f1da557a4ba57faca50c960840
agnieszka-krawczenko <krawczenkoagnieszka@gmail.com>
2019-10-02 13:23:25 +0200

    Added some if statements in test.js

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-alert("Hello, World!");
+var youLikeTomato = false;
+var myNum = 11;
+
+if (youLikeTomato) {
+    document.write("This is tomato soup.");
+}
+else {
+    document.write("You hate tomatos.");
+}
+
+if (myNum == 10){
+    document.write(" myNum is equal to 10");
+}
+else {
+    document.write(" myNum isn't equal to 10");
+}
