620fd78c08da1bae3aa9acf26df2a8694294f6c2
agnieszka-krawczenko <krawczenkoagnieszka@gmail.com>
2019-10-03 17:20:03 +0200

    Used constructor functions to create new objects

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+/*
 var myArray = new Array();
 myArray[0] = 8;
 myArray[1] = "hello";
@@ -24,3 +25,24 @@ var myCar2 = {
 myCar2.logDriver();
 console.log(myCar2.maxspeed);
 myCar2.drive(50, 3);
+*/
+
+var Car = function(maxspeed, driver){
+
+    this.maxspeed = maxspeed;
+    this.driver = driver;
+    this.drive = function(speed, time){
+        console.log(speed * time);
+    },
+    this.logDriver = function(){
+        console.log("driver name is " + this.driver);
+    };
+}
+
+var myCar = new Car(70, "Liam");
+var myCar2 = new Car(80, "Andrew");
+var myCar3 = new Car(25, "George");
+var myCar4 = new Car(90, "Bob");
+
+myCar.drive(30,5);
+myCar3.logDriver();
